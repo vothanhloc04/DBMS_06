45315cedaa25088ce4fea00fa02f91c3d37f209f
NothingToShow <74842978+pgh111222333@users.noreply.github.com>
2024-04-23 13:13:11 +0700

    proc tim hoa don

---
+++ Tim_Hoa_Don.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE TimHoaDon
+(
+    @Ngay date
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT CTDH.MaDH, CTDH.MaSP, CTDH.TrangThai, CTDH.SoLuong, CTDH.DonGia, CTDH.TongTien
+    FROM ChiTietDonHang CTDH
+    INNER JOIN DonHang DH ON DH.MaDH = CTDH.MaDH
+    WHERE CONVERT(date, DH.NgayPhucVu) = @Ngay;
+END;
